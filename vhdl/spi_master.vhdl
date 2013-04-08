208e947bacc0f2847bfb6d3a5df4cb86376865bf
makestuff <github.com@m3.ath.cx>
2013-04-08 10:37:55 +0100

    Renamed FSM states back to S_

--- vhdl/spi_master.vhdl
+++ vhdl/spi_master.vhdl
@@ -46,15 +46,15 @@ entity spi_master is
 end entity;
  
 architecture rtl of spi_master is
-	-- Sender FSM
-	type SSType is (
-		SS_IDLE,
-		SS_RECV_COUNT,
-		SS_RECV_NOCOUNT,
-		SS_SCLK_LOW,       -- drive LSB on spiData whilst holding spiClk low
-		SS_SCLK_HIGH       -- drive LSB on spiData whilst holding spiClk high
+	-- SPI master FSM
+	type StateType is (
+		S_IDLE,
+		S_RECV_COUNT,
+		S_RECV_NOCOUNT,
+		S_SCLK_LOW,       -- drive LSB on spiData whilst holding spiClk low
+		S_SCLK_HIGH       -- drive LSB on spiData whilst holding spiClk high
 	);
-	signal state, state_next            : SSType := SS_IDLE;
+	signal state, state_next            : StateType := S_IDLE;
 	signal shiftOut, shiftOut_next      : std_logic_vector(7 downto 0) := (others => '0');  -- outbound shift reg
 	signal shiftIn, shiftIn_next        : std_logic_vector(6 downto 0) := (others => '0');  -- inbound shift reg
 	signal recvData, recvData_next      : std_logic_vector(7 downto 0) := (others => '0');  -- receive side dbl.buf
@@ -67,7 +67,7 @@ begin
 	begin
 		if ( rising_edge(clk_in) ) then
 			if ( reset_in = '1' ) then
-				state <= SS_IDLE;
+				state <= S_IDLE;
 				shiftOut <= (others => '0');
 				shiftIn <= (others => '0');
 				recvData <= (others => '0');
@@ -104,40 +104,40 @@ begin
 		sendReady_out <= '0';  -- not ready for data
 		case state is
 			-- Wait for data on the send pipe for us to clock out
-			when SS_IDLE =>
+			when S_IDLE =>
 				spiClk_out  <= '1';
 				sendReady_out <= '1';  -- ready for data
 				if ( sendValid_in = '1' ) then
 					-- we've got some data to send...prepare to clock it out
-					state_next <= SS_SCLK_LOW;           -- spiClk going low
+					state_next <= S_SCLK_LOW;           -- spiClk going low
 					shiftOut_next <= sendData_in;        -- get send byte from FIFO
 					bitCount_next <= "111";              -- need eight bits for a full byte
 					cycleCount_next <= cycleCount_init;  -- initialise the delay counter
 				end if;
 
 			-- Drive bit on spiData, and hold spiClk low for cycleCount+1 cycles
-			when SS_SCLK_LOW =>
+			when S_SCLK_LOW =>
 				spiClk_out <= '0';
 				cycleCount_next <= cycleCount - 1;
 				if ( cycleCount = 0 ) then
-					-- Time to move on to SS_SCLK_HIGH - prepare to sample input data
-					state_next <= SS_SCLK_HIGH;
+					-- Time to move on to S_SCLK_HIGH - prepare to sample input data
+					state_next <= S_SCLK_HIGH;
 					shiftIn_next <= spiData_in & shiftIn(6 downto 1);
 					cycleCount_next <= cycleCount_init;
 					if ( bitCount = 0 ) then
 						-- Update the recvData register
 						recvData_next <= spiData_in & shiftIn(6 downto 0);
-						state_next <= SS_RECV_COUNT;
+						state_next <= S_RECV_COUNT;
 					end if;
 				end if;
 
 			-- Wait for consumption of received byte whilst counting down
-			when SS_RECV_COUNT =>
+			when S_RECV_COUNT =>
 				spiClk_out <= '1';
 				recvValid_out <= '1';
 				cycleCount_next <= cycleCount - 1;
 				if ( cycleCount = 0 ) then
-					state_next <= SS_RECV_NOCOUNT;
+					state_next <= S_RECV_NOCOUNT;
 				end if;
 				if ( recvReady_in = '1' ) then
 					if ( cycleCount = 0 ) then
@@ -145,29 +145,29 @@ begin
 						sendReady_out <= '1';
 						if ( sendValid_in = '1' ) then
 							-- There's a new byte to send
-							state_next <= SS_SCLK_LOW;
+							state_next <= S_SCLK_LOW;
 							shiftOut_next <= sendData_in;
 							bitCount_next <= "111";
 							cycleCount_next <= cycleCount_init;
 						else
 							-- Nothing to do, go idle
-							state_next <= SS_IDLE;
+							state_next <= S_IDLE;
 						end if;
 					else
 						-- Received byte will be consumed - go to HIGH state for remainder of count
-						state_next <= SS_SCLK_HIGH;
+						state_next <= S_SCLK_HIGH;
 					end if;
 				else
 					if ( cycleCount = 0 ) then
 						-- Received byte will not be consumed yet, and we're out of time - wait in NOCOUNT
-						state_next <= SS_RECV_NOCOUNT;
+						state_next <= S_RECV_NOCOUNT;
 					else
 						-- Received byte will not be consumed yet, but we're not yet out of time - wait here
 					end if;
 				end if;
 
 			-- Wait for consumption of received byte; no countdown
-			when SS_RECV_NOCOUNT =>
+			when S_RECV_NOCOUNT =>
 				spiClk_out <= '1';
 				recvValid_out <= '1';
 				if ( recvReady_in = '1' ) then
@@ -175,22 +175,22 @@ begin
 					sendReady_out <= '1';
 					if ( sendValid_in = '1' ) then
 						-- There's a new byte to send
-						state_next <= SS_SCLK_LOW;
+						state_next <= S_SCLK_LOW;
 						shiftOut_next <= sendData_in;
 						bitCount_next <= "111";
 						cycleCount_next <= cycleCount_init;
 					else
 						-- Nothing to do, go idle
-						state_next <= SS_IDLE;
+						state_next <= S_IDLE;
 					end if;
 				end if;
 
 			-- Carry on driving bit on spiData, hold spiClk high for four cycles
-			when SS_SCLK_HIGH =>
+			when S_SCLK_HIGH =>
 				spiClk_out <= '1';
 				cycleCount_next <= cycleCount - 1;
 				if ( cycleCount = 0 ) then
-					-- Time to move back to SS_SCLK_LOW or SS_IDLE - shift next bit out on next clock edge
+					-- Time to move back to S_SCLK_LOW or S_IDLE - shift next bit out on next clock edge
 					shiftOut_next <= "0" & shiftOut(7 downto 1);
 					bitCount_next <= bitCount - 1;
 					cycleCount_next <= cycleCount_init;
@@ -199,17 +199,17 @@ begin
 						sendReady_out <= '1';
 						if ( sendValid_in = '1' ) then
 							-- There's a new byte to send
-							state_next <= SS_SCLK_LOW;
+							state_next <= S_SCLK_LOW;
 							shiftOut_next <= sendData_in;
 							bitCount_next <= "111";
 							cycleCount_next <= cycleCount_init;
 						else
 							-- Nothing to do, go idle
-							state_next <= SS_IDLE;
+							state_next <= S_IDLE;
 						end if;
 					else
 						-- This was not the last bit...do another clock
-						state_next <= SS_SCLK_LOW;
+						state_next <= S_SCLK_LOW;
 					end if;
 				end if;
 		end case;
